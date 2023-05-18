2efcb2ac475d43657a56b3e7051fbe3188f7d950
Mdeni1234 <itsdenimartin@gmail.com>
2023-05-18 21:04:47 +0700

    fix

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -36,7 +36,7 @@
   /**
    * @type {RemoteParticipant[] | any[]}
    */
-  let remoteParticipants = [];
+  let participantVideoElements = [];
 
   async function connectToRoom() {
     const roomUrl = "wss://video-call-m23damml.livekit.cloud";
@@ -49,69 +49,123 @@
       roomName: getRoom[0].name,
       participant: "Deni",
     });
-    room
-      .on(RoomEvent.TrackSubscribed, handleTrackSubscribed)
-      .on(RoomEvent.TrackUnsubscribed, handleTrackUnsubscribed)
-      .on(RoomEvent.ActiveSpeakersChanged, handleActiveSpeakerChange)
-      .on(RoomEvent.Disconnected, handleDisconnect)
-      .on(RoomEvent.LocalTrackUnpublished, handleLocalTrackUnpublished);
 
     await room.connect(roomUrl, jwt);
-    localParticipant = room.localParticipant;
-    const videoElement = document.createElement("video");
-    videoElement.srcObject = localParticipant.videoTrack;
-    videoElement.style.width = "100%";
-    videoElement.style.height = "100%";
-    document.body.appendChild(videoElement);
-    console.log(localParticipant);
-    // publish local camera and mic tracks
     await room.localParticipant.enableCameraAndMicrophone();
+    console.log(room);
+  }
 
-    /**
-     * @param {RemoteTrack} track
-     * @param {RemoteTrackPublication} publication
-     * @param {RemoteParticipant} participant
-     */
-    function handleTrackSubscribed(track, publication, participant) {
-      console.log(track);
-      if (track.kind === Track.Kind.Video || track.kind === Track.Kind.Audio) {
-        const element = track.attach();
-        const parentElement = document.getElementById("local-video");
-        // @ts-ignore
-        parentElement.appendChild(element);
-      }
-    }
-    /**
-     * @param {RemoteTrack} track
-     * @param {RemoteTrackPublication} publication
-     * @param {RemoteParticipant} participant
-     */
-    function handleTrackUnsubscribed(track, publication, participant) {
-      // remove tracks from all attached elements
-      track.detach();
-    }
-    /**
-     * @param {LocalTrackPublication} track
-     * @param {LocalParticipant} participant
-     */
-    function handleLocalTrackUnpublished(track, participant) {
-      // when local tracks are ended, update UI to remove them from rendering
-      // track.detach();
-    }
-    /**
-     * @param {Participant[]} speakers
-     */
-    function handleActiveSpeakerChange(speakers) {
-      // show UI indicators when participant is speaking
-    }
+  /**
+   * @param {any} videoTrack
+   */
+  function displayVideoTrack(videoTrack) {
+    const videoElement = document.createElement("video");
+    // @ts-ignore
+    videoElement.srcObject = new MediaStream([videoTrack]);
+    videoElement.autoplay = true;
+    videoElement.muted = true;
 
-    function handleDisconnect() {
-      console.log("disconnected from room");
+    const videoContainer = document.getElementById("videoContainer");
+    // @ts-ignore
+    videoContainer.appendChild(videoElement);
+
+    // Menyimpan video element dalam array
+    // @ts-ignore
+    participantVideoElements.push(videoElement);
+  }
+  /**
+   * @param {RemoteTrackPublication} publication
+   * @param {RemoteParticipant} participant
+   */
+  function handleTrackPublished(publication, participant) {
+    console.log("track publised");
+    if (publication.kind === "video") {
+      const videoTrack = publication.track;
+      // @ts-ignore
+      displayVideoTrack(videoTrack);
     }
+  }
+
+  room.participants.forEach((participant) => {
+    participant.videoTracks.forEach((publication) => {
+      const videoTrack = publication.track;
+      // @ts-ignore
+      displayVideoTrack(videoTrack);
+    });
+  });
+  /**
+   * @param {RemoteTrack} track
+   * @param {RemoteTrackPublication} publication
+   * @param {RemoteParticipant} participant
+   */
+  function handleTrackSubscribed(track, publication, participant) {
+    console.log(track);
+    if (track.kind === Track.Kind.Video || track.kind === Track.Kind.Audio) {
+      const element = track.attach();
+      const parentElement = document.getElementById("local-video");
+      // @ts-ignore
+      parentElement.appendChild(element);
+    }
+  }
+  /**
+   * @param {RemoteTrack} track
+   * @param {RemoteTrackPublication} publication
+   * @param {RemoteParticipant} participant
+   */
+  function handleTrackUnsubscribed(track, publication, participant) {
+    // remove tracks from all attached elements
+    track.detach();
+  }
+  /**
+   * @param {LocalTrackPublication} track
+   * @param {LocalParticipant} participant
+   */
+  function handleLocalTrackUnpublished(track, participant) {
+    // when local tracks are ended, update UI to remove them from rendering
+    // track.detach();
+  }
+  /**
+   * @param {Participant[]} speakers
+   */
+  function handleActiveSpeakerChange(speakers) {
+    // show UI indicators when participant is speaking
+  }
+
+  function handleDisconnect() {
+    console.log("disconnected from room");
   }
 
   onMount(() => {
     connectToRoom();
+    room.on(RoomEvent.ParticipantConnected, (participant) => {
+      console.log("participant connected");
+      participant.videoTracks.forEach((publication) => {
+        const videoTrack = publication.track;
+        displayVideoTrack(videoTrack);
+      });
+    });
+
+    // Event participantDisconnected
+    room.on(RoomEvent.ParticipantDisconnected, (participant) => {
+      // Menghapus video element dari participant yang keluar dari room
+      participantVideoElements = participantVideoElements.filter(
+        (element) => element.srcObject !== participant.videoTracks
+      );
+    });
+    room
+      .on(RoomEvent.TrackUnsubscribed, handleTrackUnsubscribed)
+      .on(RoomEvent.ActiveSpeakersChanged, handleActiveSpeakerChange)
+      .on(RoomEvent.Disconnected, handleDisconnect)
+      .on(RoomEvent.LocalTrackUnpublished, handleLocalTrackUnpublished)
+      .on(RoomEvent.TrackPublished, handleTrackPublished);
+
+    // Menampilkan video participant yang telah bergabung sebelumnya di room
+    room.participants.forEach((participant) => {
+      participant.videoTracks.forEach((publication) => {
+        const videoTrack = publication.track;
+        displayVideoTrack(videoTrack);
+      });
+    });
   });
 
   onDestroy(() => {
@@ -119,37 +173,9 @@
   });
 </script>
 
-<div>
-  <h1>Video Call</h1>
-
-  <h2>Local Participant</h2>
-  {#if localParticipant && localParticipant.videoTracks}
-    <video class="participant" id="local-video" autoplay muted>
-      <track
-        kind="captions"
-        src={localParticipant.videoTrack}
-        srclang="en"
-        label="English"
-        default
-      />
-    </video>
-  {/if}
-  <h2>Remote Participants:</h2>
-  {#each remoteParticipants as participant}
-    {#if participant.videoTrack}
-      <div>
-        <h3>{participant.identity}</h3>
-        <video
-          class="remote-participant"
-          bind:this={participant.videoTrack}
-          autoplay={true}
-        >
-          <track kind="captions" srclang="en" label="English" default />
-        </video>
-      </div>
-    {/if}
-  {/each}
-</div>
+<main>
+  <div id="videoContainer" />
+</main>
 
 <style>
   .participant {
